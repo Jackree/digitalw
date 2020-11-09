c61214afd83e0d6f4d1e7083ec06005dac63e950
Dmitry Ivanov <jackree@yandex.ru>
2020-11-10 01:58:23 +0300

    Верстка страницы карьера, доработка остального кода

--- build/js/navigation.js
+++ build/js/navigation.js
@@ -1,9 +1,38 @@
 "use strict";
 
-const navButton = document.querySelector(".header__burger");
+const navButton = document.querySelector(".burger");
 const navigation = document.querySelector(".mobile-nav");
+
+let underNav = document.createElement("div");
+underNav.classList.add("under-nav");
+underNav.addEventListener("click", toggleNav);
+
+function toggleNav() {
+  navButton.classList.toggle("burger--close")
+  navigation.classList.toggle("mobile-nav--opened");
+  document.body.classList.toggle("noscroll-nav");
+
+  if (navigation.classList.contains("mobile-nav--opened")) {
+    document.querySelector(".header").append(underNav);
+  } else {
+    underNav.remove();
+  }
+}
 
 navButton.addEventListener("click", (e) => {
   e.preventDefault();
-  navigation.classList.toggle("mobile-nav--opened");
+  toggleNav();
 })
+
+const header = document.querySelector(".header");
+const headerOffset = header.offsetTop;
+
+function fixedHeader() {
+  if (window.pageYOffset > headerOffset) {
+    header.classList.add("header--fixed");
+  } else {
+    header.classList.remove("header--fixed");
+  }
+}
+
+window.addEventListener("scroll", fixedHeader);
